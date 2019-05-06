534f09caff99048c6b2941b2bfc1ffe66870b5c6
jxle <jxwang96@gmail.com>
2019-05-06 13:41:02 -0400

    add astar

---
+++ path_planning/src/path_planning/utils.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+"""
+Return the distance from node1 to node2
+"""
+def dist(node1, node2):
+    return (((node1.position[0] - node2.position[0]) ** 2) + ((node1.position[1] - node2.position[1]) ** 2)) **0.5
+
+def compute_unit_vector(node1, node2):
+    # Unit vector pointing from node1 to node2
+    s = np.array(node2.position)-np.array(node1.position)
+    s = s[0:2].flatten()
+    ns = np.linalg.norm(s)
+    if ns < 1e-10:
+        return np.array((0,0))
+    s = s/ns
+    return s
+
+def heuristic(node1, node2, AUV_speed):
+    # TODO: make max_current the actual max current
+    max_current = 10
+    speed_exaggerated = AUV_speed + max_current
+    #time_to_destination = dist(node1, node2)/speed_exaggerated
+    time_to_destination = dist(node1, node2)/AUV_speed
+    return time_to_destination
+
+def cost_function(node1, node2, AUV_speed, alpha):
+    S = AUV_speed
+    #Use the current at the first node (could also use the average between node1 and node2)"
+    V_current = node1.current
+
+    #Unit vector from node1 to node2: use only x,y components
+    s = np.array(node2.position)-np.array(node1.position)
+    s = s[0:2].flatten()
+    ns = np.linalg.norm(s)
+    if ns < 1e-10:
+        return (0, np.array((0,0)))
+    s = s/ns
+
+    c = -(V_current[0]*s[1]-V_current[1]*s[0])
+
+    # Compute V_AUV - the required AUV velocity direction to get us from node1 to node2, accounting for the current
+    # uses quadratic formula, so have to check if we want the negative or positive solution
+    V_AUVy = (np.sqrt((s[0]**2)*(s[1]**2)*(S**2)+ (s[1]**4)*(S**2) -(s[1]**2)*c**2) -s[0]*c)
+
+    V_AUVpp = np.array((np.sqrt(S**2 - V_AUVy**2),V_AUVy))
+    V_AUVpn = np.array((np.sqrt(S**2 - V_AUVy**2),-V_AUVy))
+    V_AUVnp = np.array((-np.sqrt(S**2 - V_AUVy**2),V_AUVy))
+    V_AUVnn = np.array((-np.sqrt(S**2 - V_AUVy**2),-V_AUVy))
+
+    list = [V_AUVpp,V_AUVpn, V_AUVnp,V_AUVnn]
+
+    V_AUV = list[np.argmax([np.dot(V+V_current,s) for V in list])]
+
+    # compute the speed we are travelling in the target direction, i.e. from node1 to node2
+    SpeedInTargetDirection = np.dot(V_AUV,s) + np.dot(V_current,s)
+
+    # compute the time it will take to travel from node1 to node2 - handle edge case that 0 speed=inf time
+    time = dist(node1,node2)/SpeedInTargetDirection if SpeedInTargetDirection != 0 else 0
+    timeToDestination = (time if time > 0  else np.inf)
+    
+    riskfactor = (1/(1-alpha*node2.risk + 1e-10))
+    
+    return riskfactor*timeToDestination, V_AUV
